5366aa1a96738fd98e51fcf19447bd64842806a4
WillianJefferson <willian100@gmail.com>
2025-04-02 19:35:37 -0300

    Atualizando tarefa 4

---
+++ Aula 1/Tarefa - 4.py
@@ -0,0 +1,11 @@
+nota1 = 0
+nota2 = 0
+nota3 = 0
+nota4 = 0
+
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+nota3 = float(input("Informe a nota 3: "))
+nota4 = float(input("Informe a nota 4: "))
+
+print("A média das notas é igual: ", (nota1 + nota2 + nota3 + nota4)/4)
